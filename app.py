6d490db7d49f68d176c6cefd409cb2b5d9ab9a7b
Anton Edeh <edeha@cardiff.ac.uk>
2023-06-19 03:56:00 +0500

    Started implementing Interface

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
+import os
+from dotenv import load_dotenv
+import streamlit as st
+from PyPDF2 import PdfFileReader
+
 def main():
-    print('Hello World!')
+    load_dotenv()
+    st.set_page_config(page_title='PandaPDF', page_icon=':panda_face:')
+    st.header('PandaPDF 🐼')
+    st.subheader('Your PDF to Chat Converter')
+    st.write('👉 This is a simple web app that converts your PDF to a chat format.') 
+    uploaded_file = st.file_uploader("Upload your File 📁", type=['pdf'])
+   
+
 
 
 if __name__ == '__main__':
